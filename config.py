a368a82aad8c31dd36307e8f4fef38e4676e9356
tejas-mn <tejasmn2020@gmail.com>
2023-05-21 18:06:27 +0530

    made some changes

--- config.py
+++ config.py
@@ -1,16 +1,13 @@
 from flask import Flask
-# from flask_cors import CORS,cross_origin
 from routes import main
 from delete import scheduleDelete
 
 def create_app():
     app = Flask(__name__)
-    # cors = CORS(app)
     app.config["SECRET_KEY"] = "123"
+    app.config['DEBUG'] = True
     app.register_blueprint(main)
-
-    # scheduleDelete('static', 2) #delete every 1 min | comment to stop tests
-
+    scheduleDelete('static', 2) #delete every 1 min | comment to stop tests
     return app
 
 
